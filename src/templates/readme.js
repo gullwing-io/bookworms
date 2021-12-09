7c9dc175c476ed6b46f389f35dc3e21e63169cee
Robin Glen <robinglen@gmail.com>
2021-12-09 09:12:13 +0000

    remove need for a title

--- src/templates/readme.js
+++ src/templates/readme.js
@@ -1,46 +1,61 @@
 const bookmark = (timeDate, label, description, children) => {
-    return `
-# ${label}
+  return `
+${addTitle(label)}
 ${addDescription(description)}
 ${children}
 ${addBookwormsDescription(timeDate)}
-`
-}
+`;
+};
 
 const bookmarkFolder = (index, label, description, children) => {
-    return `
+  return `
 ${convertNumberIntoHeader(index, label)}
 ${addDescription(description)}
 ${children}
-    `
-}
+    `;
+};
 
 const convertNumberIntoHeader = (index, label) => {
-    const arr = ['']
-    for (let i = 0; i <= index; i++) {
-        arr.push('#')
-    }
-    return `${arr.join('')} ${label}`
-}
-
+  const arr = [""];
+  for (let i = 0; i <= index; i++) {
+    arr.push("#");
+  }
+  return `${arr.join("")} ${label}`;
+};
 
 const bookmarkLink = (label, description, href) => {
-    return `
+  return `
 * [${label}](${href}) - ${addDescription(description)}
-`
-}
+`;
+};
+
+const addTitle = (title) => {
+  if (title && title !== "") {
+    return `# ${title}`;
+  } else {
+    return ``;
+  }
+};
 
 const addDescription = (description) => {
-    if (description) {
-        return `${description}`
-    } else {
-        return ``
-    }
-}
+  if (description) {
+    return `${description}`;
+  } else {
+    return ``;
+  }
+};
 
 const addBookwormsDescription = (epoch) => {
-    const date = new Date(epoch).toLocaleString()
-    return `_These bookmarks were last updated on ${date} using [Bookworms](https://github.com/thearegee/bookworms)_`
-}
+  const date = new Date(epoch).toLocaleString();
+  return `_These bookmarks were last updated on ${date} using [Bookworms](https://github.com/thearegee/bookworms)_`;
+};
 
-export {bookmark, bookmarkFolder, convertNumberIntoHeader, bookmarkLink, addDescription, addBookwormsDescription}
+export {
+  bookmark,
+  bookmarkFolder,
+  convertNumberIntoHeader,
+  bookmarkLink,
+  addTitle,
+  addDescription,
+  addBookwormsDescription,
+};
